448b4c58bde16f7dc5ebf100482c1c48b4cd2ba2
notsupposed <notsupposed@protonmail.com>
2018-12-24 20:16:43 +0300

    Add JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
+const ESC_KEY_CODE = 27;
 const btnFeedback = document.querySelector('.contacts a.button');
-const modalContact = document.querySelector('.modal-login');
+const modalFeedback = document.querySelector('.modal-feedback');
+const closeFeedback = document.querySelector('.modal-feedback .modal-close');
 
 const linkMap = document.querySelector('.contacts a.map');
+const modalMap = document.querySelector('.modal-map');
+const closeMap = document.querySelector('.modal-map .modal-close');
 
-btnContacts.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
+btnFeedback.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalFeedback.classList.add("modal-show");
+  modalFeedback.querySelector('#feedback-name').focus();
+});
+
+closeFeedback.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalFeedback.classList.remove("modal-show");
+});
+
+linkMap.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalMap.classList.add("modal-show");
+});
+
+closeMap.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalMap.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode !== ESC_KEY_CODE) return;
+  const modal = document.querySelector('.modal-show');
+  console.log(modal);
+  if (modal) {
+    modal.classList.remove("modal-show");
+    e.preventDefault;
+  }
 });
