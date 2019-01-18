5945288964e91ac908fcc521663049d860970f52
notsupposed <notsupposed@protonmail.com>
2019-01-18 09:16:26 +0300

    Force single quotes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const ESC_KEY_CODE = 27;
+
 const btnFeedback = document.querySelector('.contacts a.button');
 const modalFeedback = document.querySelector('.modal-feedback');
 const closeFeedback = document.querySelector('.modal-feedback .modal-close');
@@ -7,33 +8,32 @@ const linkMap = document.querySelector('.contacts a.map');
 const modalMap = document.querySelector('.modal-map');
 const closeMap = document.querySelector('.modal-map .modal-close');
 
-btnFeedback.addEventListener("click", function (e) {
+btnFeedback.addEventListener('click', function (e) {
   e.preventDefault();
-  modalFeedback.classList.add("modal-show");
+  modalFeedback.classList.add('modal-show');
   modalFeedback.querySelector('#feedback-name').focus();
 });
 
-closeFeedback.addEventListener("click", function (e) {
+closeFeedback.addEventListener('click', function (e) {
   e.preventDefault();
-  modalFeedback.classList.remove("modal-show");
+  modalFeedback.classList.remove('modal-show');
 });
 
-linkMap.addEventListener("click", function (e) {
+linkMap.addEventListener('click', function (e) {
   e.preventDefault();
-  modalMap.classList.add("modal-show");
+  modalMap.classList.add('modal-show');
 });
 
-closeMap.addEventListener("click", function (e) {
+closeMap.addEventListener('click', function (e) {
   e.preventDefault();
-  modalMap.classList.remove("modal-show");
+  modalMap.classList.remove('modal-show');
 });
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener('keydown', function (e) {
   if (e.keyCode !== ESC_KEY_CODE) return;
   const modal = document.querySelector('.modal-show');
-  console.log(modal);
   if (modal) {
-    modal.classList.remove("modal-show");
-    e.preventDefault;
+    e.preventDefault();
+    modal.classList.remove('modal-show');
   }
 });
